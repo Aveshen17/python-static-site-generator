d8226198b856f3d16f1e3bee5595888c13648035
Aveshen17 <81326165+Aveshen17@users.noreply.github.com>
2021-04-20 08:16:46 +0200

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,7 +15,7 @@ class Parser:
         with open(path, "r",) as file:
             return file.read()
 
-    def write(self, path: Path, dest: Path, content: Path):
+    def write(self, path: Path, dest: Path, content: Path, ext=".html"):
         full_path=dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
@@ -23,7 +23,7 @@ class Parser:
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-    class ResourceParser(Parser):
+class ResourceParser(Parser):
         extensions = [".jpg", ",png",".gif",".css",".html" ]
 
     def parse(self, path, source, dest):
