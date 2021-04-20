7f92bf2cc7865fe9e910031d53edb0e054245548
Aveshen17 <81326165+Aveshen17@users.noreply.github.com>
2021-04-20 07:59:11 +0200

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -3,7 +3,7 @@ import re
 from yaml import load
 from yaml import FullLoader
 
-Class Content(Mapping):
+class Content(Mapping):
     __delimiter= r"^(?:-|\+){3}\s*$"
     __regex=re.compile(__delimiter, re.MULTILINE)
 
